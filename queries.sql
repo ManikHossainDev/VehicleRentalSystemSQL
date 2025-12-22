fa0f46b6b0bf9be2ca9556831dd935b7333cd274
Programmer-Manik <programmermanik1502083874@gmail.com>
2025-12-23 02:20:37 +0600

    inner join addd

--- queries.sql
+++ queries.sql
@@ -36,7 +36,14 @@ values
   );
 
 -- select users table data 
-select * from users;
+select
+  *
+from
+  users;
+
+
+
+
 
 -- create vehicles table 
 create table
@@ -53,16 +60,20 @@ create table
       rental_price decimal(10, 2) not null,
       status varchar(20) not null check (status in ('available', 'rented', 'maintenance'))
   );
+
+
+
 -- insert table data  
-insert into vehicles (
-  name,
-  type
+insert into
+  vehicles (
+    name,
+    type
 ,
-    model,
-    registration_number,
-    rental_price,
-    status
-)
+      model,
+      registration_number,
+      rental_price,
+      status
+  )
 values
   (
     'Toyota Corolla',
@@ -98,4 +109,89 @@ values
   );
 
 -- select usinge all vehicles data get 
-select * from vehicles;
+select
+  *
+from
+  vehicles;
+
+
+
+
+-- create bookings table 
+create table
+  bookings (
+    booking_id serial primary key,
+    user_id int not null references users (user_id),
+    vehicle_id int not null references vehicles (vehicle_id),
+    start_date date not null,
+    end_date date not null,
+    booking_status varchar(20) not null check (
+      booking_status in ('pending', 'confirmed', 'completed', 'cancelled')
+    ),
+    total_cost decimal(10, 2) not null
+  );
+
+-- insert bookings table data
+insert into
+  bookings (
+    user_id,
+    vehicle_id,
+    start_date,
+    end_date,
+    booking_status,
+    total_cost
+  )
+values
+  (
+    1,
+    2,
+    '2023-10-01',
+    '2023-10-05',
+    'completed',
+    240.00
+  ),
+  (
+    1,
+    2,
+    '2023-11-01',
+    '2023-11-03',
+    'completed',
+    120.00
+  ),
+  (
+    3,
+    2,
+    '2023-12-01',
+    '2023-12-02',
+    'confirmed',
+    60.00
+  ),
+  (
+    1,
+    1,
+    '2023-12-10',
+    '2023-12-12',
+    'pending',
+    100.00
+  );
+
+-- select bookings data
+select
+  *
+from
+  bookings; 
+
+
+
+-- Query 1: JOIN
+select 
+  b.booking_id as booking_id,
+  u.name as customer_name,
+  v.name as vehicle_name,
+  b.start_date,
+  b.end_date,
+  b.booking_status as status
+from bookings b
+inner join users u on b.user_id = u.user_id
+inner join vehicles v on b.vehicle_id = v.vehicle_id;
+-- 
